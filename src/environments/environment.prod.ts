0a032bd7c94c07cf0e2843ca0637cd2356b34a9e
ayushkushwaha01 <ayush@optionmatrix.com>
2026-08-12 15:27:51 +0530

    fixed for dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
-export const environment = {
-  production: true,
-  mode: 1,
-  // apiUrl : 'https://mmdevapi.omsaiintex.co.in/'
-//  apiUrl: 'https://qasqaapi.optionmatrix.co.in/api/' // <-- Added api/ here
+// export const environment = {
+//   production: true,
+//   mode: 1,
+//   // apiUrl : 'https://mmdevapi.omsaiintex.co.in/'
+// //  apiUrl: 'https://qasqaapi.optionmatrix.co.in/api/' // <-- Added api/ here
 
-};
+// };
 
 
 // For Upload
